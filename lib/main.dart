b2e919ec949f73b13b03febfb0e53185ce0913c8
DEEP DAS <deepdassonu.ktr@gmail.com>
2022-01-24 13:35:43 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 
 void main() {
@@ -13,8 +14,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-      
-    );
+      // home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()     
+      },
+    ); 
   }
 }
