2183a8ad20fba608ebe2ccbc414551156f304ddf
DEEP DAS <deepdassonu.ktr@gmail.com>
2022-02-13 14:12:51 +0530

    Day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,35 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 // ignore_for_file: prefer_const_constructors
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogjson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogjson);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]); 
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
@@ -21,7 +39,8 @@ class HomePage extends StatelessWidget {
         child: ListView.builder(
           itemCount: dummyList.length,
           itemBuilder: (context, index) {
-            return ItemWidget(item: dummyList[index],
+            return ItemWidget(
+              item: dummyList[index],
             );
           },
         ),
