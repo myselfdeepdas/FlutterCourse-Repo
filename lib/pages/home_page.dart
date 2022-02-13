f2daaf89c2c6b3099d80906bf2582440c567e6ce
DEEP DAS <deepdassonu.ktr@gmail.com>
2022-02-13 13:42:24 +0530

    Day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 // ignore_for_file: prefer_const_constructors
 
 class HomePage extends StatelessWidget {
@@ -9,17 +11,19 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(   
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]); 
+    return Scaffold(
       appBar: AppBar(
-       title: Text("Catalog App"),
+        title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to Home Page",  style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25
-            
-            ),),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
